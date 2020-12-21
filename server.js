a3e5d00e1cade78fb4f6217a0fc00821f261a7c8
Sangraam Patwardhan <sangraampatwardhan@Sangraams-MacBook-Pro.local>
2020-12-21 14:27:03 +0530

    Added bootcamp  routes, used controllers to restructure the code

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
 const express = require('express')
 const dotenv = require('dotenv')
+const morgan = require('morgan')
+
+// Route files
+const bootcampsRouter = require('./routes/bootcamps')
 
 // load env vars
 dotenv.config({ path: './config/config.env' })
 
 const app = express()
 
+// Dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+	app.use(morgan('dev'))
+}
+
+app.use('/api/v1/bootcamps', bootcampsRouter)
+
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}.`))
+app.listen(
+	PORT,
+	console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}.`)
+)
